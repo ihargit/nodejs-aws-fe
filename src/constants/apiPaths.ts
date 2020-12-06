7743189fe8a58c906b2139b92f6ea56430e947a2
ihargit <ihar.kharashun@gmail.com>
2020-12-07 02:24:04 +0300

    feat: add order endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 
 const API_PATHS = {
   product: 'https://codrlwgu06.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'http://ihargit-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
   import: 'https://vbe3615vrf.execute-api.eu-west-1.amazonaws.com/dev/',
   bff: 'https://codrlwgu06.execute-api.eu-west-1.amazonaws.com/dev/'
 };
